6fcdcdef135f9d3d863caaac4ef3953ad61a1432
happysonsad <[kinchun917@gmail.com]>
2024-08-28 11:50:12 +0800

    solved the problem of overlap grid

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-// Create a webpage with a 16x16 grid of square divs
-
-// create a div first, then loop it?
-
 const gridContainer = document.querySelector("#grid-container");
 
 for (i = 0; i < (16 * 16); i++) {
     const div = document.createElement("div");
     div.setAttribute("id", "div-box");
     div.style.padding = "20px";
-    div.style.border = "solid 1px";
+    div.style.outline = "solid 1px";
+    div.style.marginTop = "1px";
+    div.style.marginLeft =  "1px";
     gridContainer.appendChild(div);
-
+    div.addEventListener("click", () => {
+        div.style.backgroundColor = "red";
+    });
 }
 
-// set max size of the container?
 
+// Set up a “hover” effect
+// so that the grid divs change color 
+// when your mouse passes over them
+// leaving a (pixelated) trail through your grid like a pen would
